250ed3e68669041cce928d3e72c2274ec97a8f7b
Jeremy Fumeron <jeremy.fumeron@orange.com>
2020-04-10 11:38:21 +0200

    update page

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express')
 
 const app = express()
 
+// Permet d'utiliser PUG pour la génération du HTML
+// Va faire le lien directement avec le dossier view
+app.set('view engine', 'pug')
+
+// Avec cette ligne on peut donc naviguer dans nos dossiers (à partir du public) depuis le naigateur
+// exemple: http://localhost:3003/css/sanitize.css
+app.use(express.static('public'))
+
 app.get('/', (_, res) => {
-  res.send('Hello World')
+  // res.send('Hello World !!')
+  res.render('contact', { title: 'Contact' })
 })
+
+app.listen(3003, () => console.log('server ON'))
